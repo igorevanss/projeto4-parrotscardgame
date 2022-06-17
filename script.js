ce07ec7a210da409a6addba7f121fed74bc56f6a
igorevanss <igorevans.pro@gmail.com>
2022-06-17 13:31:51 -0300

    lógica de virar carta com click feita

--- script.js
+++ script.js
@@ -19,41 +19,51 @@ function askAmount() {
     amountCards % 2 !== 0 ||
     isNaN(amountCards)
   ) {
-    alert('Digite um número par, entre 4 e 14!')
+    alert('Esta opção não é válida. Digite um número par, entre 4 e 14!')
 
     amountCards = prompt('Com quantas cartas quer jogar?')
   }
 }
+askAmount()
 
 function generateDeck() {
-  for (let i = 0; 1 < (amountCards / 2); i++) {
-    const card = cards[i];
-    deck.push(card);
-    deck.push(card);
+  for (let i = 0; i < amountCards / 2; i++) {
+    const card = cards[i]
+    deck.push(card)
+    deck.push(card)
   }
-  console.log(deck)
+
   deck.sort(shuffle)
-  console.log(deck)
-  //dealCards()
+  dealCards()
 }
+generateDeck()
 
 function shuffle() {
   return Math.random() - 0.5
 }
 
-askAmount()
-generateDeck()
-/*
 function dealCards() {
   const board = document.querySelector('.board')
 
   for (let i = 0; i < deck.length; i++) {
     let cardTemplate = `
-  <div class="card" oncliclick="">
-    <img src="./images/front.png" alt="">
-    <img src="./images/${deck[0]}.gif" alt="">
-  </div>`
+  <div class="flip-container" onclick="flipCard(this)">
+          <div class="card">
+            <div class="front">
+              <img src="./images/front.png" alt="" />
+            </div>
+
+            <div class="back">
+              <img src="./images/${deck[i]}.gif" alt="" />
+            </div>
+          </div>
+        </div>`
     board.innerHTML += cardTemplate
   }
 }
-*/
+
+function flipCard(clicked) {
+  const flip = document.querySelector('.flip-container')
+
+  clicked.classList.add('clicked')
+}
